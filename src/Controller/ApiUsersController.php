c0613030e177d48b8e4d31ed14849bbc1911f768
Elisee-des <esabidani@gmail.com>
2022-04-23 23:38:43 +0000

    _Update des Users et FetchAll et FetchbyUser

--- src/Controller/ApiUsersController.php
+++ src/Controller/ApiUsersController.php
@@ -6,7 +6,7 @@ use App\Entity\Users;
 use App\Repository\UsersRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request as Resquest;
+use Symfony\Component\HttpFoundation\Request as Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,55 +15,87 @@ class ApiUsersController extends AbstractController
     /**
      * @Route("/api/create_users", name="api_users_create", methods={"POST"})
      */
-    public function create_users(Resquest $request, ManagerRegistry $managerRegistry): Response
+    public function create_users(Request $request, ManagerRegistry $managerRegistry): Response
     {
         $data = $request->getContent();
         $datajson = json_decode($data);
 
         if (
-            property_exists($datajson, "username") && 
-            property_exists($datajson, "password") && 
+            property_exists($datajson, "username") &&
+            property_exists($datajson, "password") &&
             property_exists($datajson, "email")
         ) {
             $user = new Users();
             $user->setUsername($datajson->username)
                 ->setPassword($datajson->password)
                 ->setEmail($datajson->email);
-    
+
             $em = $managerRegistry->getManager();
             $em->persist($user);
             $em->flush();
-    
-    
-            return $this->json(["Success" => true, "data" => $data], 200);
 
+
+            return $this->json(["Success" => true, "data" => $data], 200);
         }
         return $this->json(["Success" => false, "data" => $data], 401);
     }
 
     /**
-     * @Route("/api/update_users", name="api_update_users", methods={"PUT"})
+     * @Route("/api/update_users/{id}", name="api_update_users", methods={"PUT"})
      */
     public function update_users($id, Request $request, ManagerRegistry $managerRegistry, UsersRepository $usersRepo): Response
     {
         $user = $usersRepo->find($id);
 
+        $data = $request->getContent();
+        $datajson = json_decode($data);
+
+        if (
+            property_exists($datajson, "username") &&
+            property_exists($datajson, "password") &&
+            property_exists($datajson, "email")
+        ) {
+            $user->setUsername($datajson->username)
+                ->setPassword($datajson->password)
+                ->setEmail($datajson->email);
+
+            $em = $managerRegistry->getManager();
+            $em->persist($user);
+            $em->flush();
 
 
-        $user = $request->getContent();
-        $datajson = json_decode($user);
+            return $this->json(["Success" => true, "data" => $user], 201);
+        }
 
+        return $this->json(["Success" => false, "data" => $user], 401);
+    }
 
-        $user = new Users();
-        $user->setUsername($datajson->username)
-            ->setPassword($datajson->password)
-            ->setEmail($datajson->email);
+    /**
+     * @Route("/api/fetch_users/", name="api_fetchAll_users", methods={"GET"})
+     */
+    public function fetchAll_users(UsersRepository $usersRepo): Response
+    {
+        $user = $usersRepo->findAll();
 
-        $em = $managerRegistry->getManager();
-        $em->persist($user);
-        $em->flush();
+        return $this->json(["Success" => true, "data" => $user], 200);
+    }
 
+    /**
+     * @Route("/api/fetch_users/{id}", name="api_fetch_users", methods={"GET"})
+     */
+    public function fetch_users($id, UsersRepository $usersRepo): Response
+    {
+        $user = $usersRepo->find($id);
+        $datajson = json_encode($user);
+        if (
+            property_exists($datajson, "username") &&
+            property_exists($datajson, "password") &&
+            property_exists($datajson,"email")
+        ) {
+            
+            return $this->json(["Success" => true, "data" => $user], 200);
+        }
 
-        return $this->json(["Success" => true, "data" => $user]);
+        return $this->json(["Success" => false, "data" => "Cette personne n'existe pas"], 404);
     }
 }
